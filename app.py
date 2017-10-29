44e0ce0fdf3d3f6cc7f9fe1b6f64b8b63d2e0c83
Ben Whittle <benwhittle31@gmail.com>
2017-10-29 16:32:30 -0700

    passing features into predict method; updating logging

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-import os
-import json
 from horizon_py import DeepHorizon
 
+dh_client = DeepHorizon()
 
-dh_client = DeepHorizon(
-  app_token=os.environ.get('DEEP_HORIZON_TOKEN'),
-  app_secret=os.environ.get('DEEP_HORIZON_SECRET')
-)
+features = {
+  'some-feature': 'some-val'
+}
 
-resp = dh_client.predict()
-resp_body = json.loads(resp.content)
+resp = dh_client.predict(features)
 
-print resp_body
-
-if resp_body.get('ok'):
-  price = resp_body['prediction']
-  print price
+if resp.get('ok'):
+  prediction = resp['prediction']
+  print('Got Prediction: {}'.format(prediction))
+else:
+  err = resp['error']
+  print('Prediction Error: {}'.format(err))
